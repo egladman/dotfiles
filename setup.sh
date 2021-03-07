cb710b85392640eca7d8cab2187517a94aa981e9
Eli Gladman <8713153+egladman@users.noreply.github.com>
2021-03-07 12:09:59 -0500

    support restow

--- setup.sh
+++ setup.sh
@@ -1,19 +1,47 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-stow_opts=''
+self_version=0.1.0
+
+stow_opts=(
+    '-v'
+)
+
+read -r -d '' self_usage <<-EOF
+Wrapper for GNU Stow to simplify dotfile management
+Usage: ${0##*/} [option]
+
+OPTIONS
+   -h, --help                     show this help text, and exit
+   -V, --version                  show version, and exit
+   -R, --restow                   Restow dotfiles (first unstow, then stow again). This is
+                                  useful for pruning obsolete symlinks
+   -D, --delete                   Unstow dotfiles
+EOF
 
 case "$1" in
     '')
-	printf '%s\n' "Stowing..."
-	;;
+	      printf '%s\n' "Stowing..."
+	      ;;
+    -R|--restow)
+	      printf '%s\n' "Restowing..."
+	      stow_opts=("${stow_opts[@]}" '-R')
+	      ;;
     -D|--delete)
-	printf '%s\n' "Deleting..."
-	stow_opts='-D'
-	;;
+	      printf '%s\n' "Deleting..."
+	      stow_opts=("${stow_opts[@]}" '-D')
+	      ;;
+    -V|--version)
+	      printf '%s\n' "$self_version"
+	      exit 0
+	      ;;
+    -h|--help)
+	      printf '%s\n' "$self_usage"
+	      exit 0
+	      ;;
     *)
-	printf '%s\n' "Unsupported option"
-	exit 1
-	;;
+	      printf '%s\n' "Unsupported option"
+	      exit 1
+	      ;;
 esac
 
 declare -a dir_names
@@ -22,7 +50,7 @@ if [ $? -ne 0 ]; then
     printf '%s\n' "Command 'find' returned non-zero code"
 fi
 
-stow "$stow_opts" "${dir_names[@]}" || {
+stow ${stow_opts[@]} ${dir_names[@]} || {
     printf '%s\n' "Command 'stow' returned non-zero code"
     exit 1
 }
