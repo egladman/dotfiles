07b2ddac5979f29a0c5e4bb4dfcaab0098b554a2
Eli Gladman <8713153+egladman@users.noreply.github.com>
2021-04-23 13:17:06 -0400

    add dry run flag

--- stow.sh
+++ stow.sh
@@ -1,48 +1,79 @@
 #!/usr/bin/env bash
 
-self_version=0.1.0
-log_level=0
+_version=0.1.1
+_usage=''
+_log_level=${STOWSH_LOG_LEVEL:-40}
+
+# Level          Numeric
+# verbose        5
+# debug          10
+# info           20
+# error          40
+
 stow_opts=(
-    '-v'
+    # '--adopt'
 )
 
-read -r -d '' self_usage <<-EOF
+if [[ $_log_level -le 10 ]]; then
+    stow_opts+=('-v')
+fi
+
+read -r -d '' _usage <<-EOF
 Wrapper for GNU Stow to simplify dotfile management
 Usage: ${0##*/} [option]
 
 OPTIONS
-   -h, --help                     show this help text, and exit
-   -V, --version                  show version, and exit
+   -h, --help                     Show this help text, and exit
+   -V, --version                  Show version, and exit
+   -S, --stow                     Stow dotfiles (this is the default action and can be omitted)
    -R, --restow                   Restow dotfiles (first unstow, then stow again). This is
                                   useful for pruning obsolete symlinks
    -D, --delete                   Unstow dotfiles
+   -u, --dry-run                  Do not perform any operations that modify the filesystem; merely
+                                  show what would happen.
 EOF
 
-case "$1" in
-    '')
-	      printf '%s\n' "Stowing..."
-	      ;;
-    -R|--restow)
-	      printf '%s\n' "Restowing..."
-	      stow_opts=("${stow_opts[@]}" '-R')
-	      ;;
-    -D|--delete)
-	      printf '%s\n' "Deleting..."
-	      stow_opts=("${stow_opts[@]}" '-D')
-	      ;;
-    -V|--version)
-	      printf '%s\n' "$self_version"
-	      exit 0
-	      ;;
-    -h|--help)
-	      printf '%s\n' "$self_usage"
-	      exit 0
-	      ;;
-    *)
-	      printf '%s\n' "Unsupported option"
-	      exit 1
-	      ;;
-esac
+action_count=0
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+	''|-S|--stow)
+	    [[ $_log_level -le 20 ]] && printf '%s\n' "Stowing..."
+	    action_count=$((action_count+1))
+	    ;;
+	-R|--restow)
+	    [[ $_log_level -le 20 ]] && printf '%s\n' "Restowing..."
+	    stow_opts+=('-R')
+	    action_count=$((action_count+1))
+	    ;;
+	-D|--delete)
+	    [[ $_log_level -le 20 ]] && printf '%s\n' "Deleting..."
+	    stow_opts+=('-D')
+	    action_count=$((action_count+1))
+	    ;;
+	-V|--version)
+	    printf '%s\n' "$_version"
+	    exit 0
+	    ;;
+	-h|--help)
+	    printf '%s\n' "$_usage"
+	    exit 0
+	    ;;
+	-u|--dry-run)
+	    [[ $_log_level -le 20 ]] && printf '%s\n' "Performing dry run..."
+	    stow_opts+=('--no')
+	    ;;
+	*)
+	    [[ $_log_level -le 40 ]] && printf '%s\n' "Unsupported option"
+	    exit 1
+	    ;;
+    esac
+    shift
+done
+
+if [[ $action_count -gt 1 ]]; then
+    [[ $_log_level -le 40 ]] && printf '%s\n' "Conflicting options present"
+    exit 1
+fi
 
 declare -a submodules
 declare -a packages
@@ -62,28 +93,28 @@ for path in **; do
 done
 shopt -u globstar # Disable
 
-if [[ $log_level -gt 0 ]]; then
+if [[ $_log_level -le 20 ]]; then
     printf '%s\n' "Found packages:"
     printf '  %s\n' ${packages[@]}
 fi
 
 for path in "${packages[@]}"; do
-    [[ $log_level -gt 0 ]] && printf '%s\n' "Traversing path '${path%/*}'"
+    [[ $_log_level -le 20 ]] && printf '%s\n' "Traversing path '${path%/*}'"
 
     if [[ "$path" == *\/* ]]; then
-        [[ $log_level -gt 0 ]] && printf '%s\n' "Entering directory '${path%/*}'"
+        [[ $_log_level -le 5 ]] && printf '%s\n' "Entering directory '${path%/*}'"
         pushd "${path%/*}" &> /dev/null
-        stow ${stow_opts[@]} -t "${HOME:?}" "${path#*/}"
+        stow "${stow_opts[@]}" -t "${HOME:?}" "${path#*/}"
         return_code=$?
         popd &> /dev/null
-        [[ $log_level -gt 0 ]] && printf '%s\n' "Exiting directory '${path%/*}'"
+        [[ $_log_level -le 5 ]] && printf '%s\n' "Exiting directory '${path%/*}'"
     else
         stow ${stow_opts[@]} "$path"
         return_code=$?
     fi
 
     if [[ $return_code -ne 0 ]]; then
-        printf '%s\n' "Command 'stow' returned non-zero code"
+        [[ $_log_level -le 40 ]] && printf '%s\n' "Command 'stow' returned non-zero code"
         exit 1
     fi
     unset return_code
