6238696a0b9c612f588b7c6c24f496d1330342c6
Eli Gladman <8713153+egladman@users.noreply.github.com>
2022-05-22 01:10:19 -0400

    remove 1AM debug lines...

--- i3/.config/i3/picom-toggle.sh
+++ i3/.config/i3/picom-toggle.sh
@@ -2,14 +2,10 @@
 
 # Modified fromy https://github.com/arcolinux/arcolinux-i3wm/blob/master/etc/skel/.config/i3/scripts/picom-toggle.sh
 
-echo yeet
-
 if pgrep -x "picom"  > /dev/null 2>&1; then
     # Kill
-    printf '%s\n' "Stopping"
     killall -q picom
 else
     # Daemonize
-        printf '%s\n' "Starting"
     picom -b --experimental-backends
 fi
