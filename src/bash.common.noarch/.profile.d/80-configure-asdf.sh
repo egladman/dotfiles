5bbb463229a37a1cec8ebc7efaecf98bbb6811a7
Eli Gladman <8713153+egladman@users.noreply.github.com>
2022-06-22 20:44:12 -0400

    make asdf a soft dep

--- src/bash.common.noarch/.profile.d/80-configure-asdf.sh
+++ src/bash.common.noarch/.profile.d/80-configure-asdf.sh
@@ -1,2 +1,4 @@
-source "${HOME:?}/.asdf/asdf.sh"
-source "${HOME:?}/.asdf/completions/asdf.bash"
+if [[ -d "${HOME:?}/.asdf" ]]; then
+    source "${HOME:?}/.asdf/asdf.sh"
+    source "${HOME:?}/.asdf/completions/asdf.bash"
+fi
